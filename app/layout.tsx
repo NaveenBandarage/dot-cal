ee1b33db248df85471a53e1cd9b9b466ab1d5795
Naveen Bandarage <thebanterage@gmail.com>
2025-02-13 18:47:51 +1100

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import type React from "react"; // Added import for React
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
